59d7bd3200bad70bb18ba655aa9bf8e90d62aeb1
yoyodes1000 <yoyodes1000@gmail.com>
2024-09-24 12:45:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wild-blog';
+  title = 'Bienvenue sur le Wild Blog de Pimpoye';
 }
