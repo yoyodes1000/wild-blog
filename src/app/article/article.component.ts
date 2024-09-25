6b916ddff5b5ea43cce6213b8505426f98323271
yoyodes1000 <yoyodes1000@gmail.com>
2024-09-25 14:34:46 +0200

    quest 3 OK

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
 
+  article = {
+    title: 'Titre de l\'article',
+    author: 'John Doe',
+    content: 'Voici le contenu de l\'article.',
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: ''
+  };
+
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
